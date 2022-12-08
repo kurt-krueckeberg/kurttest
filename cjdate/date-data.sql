437129d63a70c250821bfbe45471211070e4db4d
kurt-krueckeberg <kurt.krueckeberg@gmail.com>
2022-12-08 10:21:04 -0500

    $1

---
+++ cjdate/date-data.sql
@@ -0,0 +1,18 @@
+
+| SNO   | SNAME   | STATUS   | CITY     
+
+S1, Smith, 20, London
+S2, Jones, 10, Paris
+S3, Blake, 30, Paris
+S4, Clark, 20, London
+
+
+| PNO   | QTY   |
+
+| P1    | 300   |
+| P2    | 200   |
+| P2    | 400   |
+| P3    | 400   |
+| P4    | 200   |
+| P5    | 100   |
+| P6    | 100   |
--- cjdate/date.sql
+++ cjdate/date.sql
@@ -1,22 +1,32 @@
-create table S (
-SNO CHAR[2],
-SNAME VARCHAR[8],
-STATUS int,
-CITY VARCHAR[8],
-primary key(SNO)
+create database sample;
+
+create table s (
+   sno CHAR[2],
+   sname VARCHAR[8],
+   status int,
+   city VARCHAR[8],
+primary key(sno)
 );
 
-create table P (
-PNO CHAR[2],
-PNAME CHAR  ,
-COLOR VARCHAR[8] ,
-WEIGHT int,
-CITY VARCHAR[8]
+insert into s values();
+
+create table p (
+   pno CHAR[2],
+   pname CHAR,
+   color VARCHAR[8],
+   weight int,
+   city VARCHAR[8],
+primary key(pno)
 );
 
-create table SP (
-SNO CHAR  ,
-PNO CHAR  ,
-QTY CHAR  );
+insert into p values();
+
+create table sp (
+   sno CHAR,
+   pno CHAR,
+   qty CHAR,
+primary key(sno)
+);
 
 
+insert into sp values();
